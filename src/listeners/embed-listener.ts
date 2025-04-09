e39cd0b4650f9b5b939d4d353d6e09556d69ddd6
Eric Heinze <eheinze920@gmail.com>
2025-04-09 11:49:46 -0600

    added some more env vars and fixed footer to include NY time

--- src/listeners/embed-listener.ts
+++ src/listeners/embed-listener.ts
@@ -6,6 +6,7 @@ import {
   ThreadChannel,
 } from "discord.js";
 import { getOpenAiResponse } from "../lib/openai";
+import { DateTime } from "luxon";
 
 export class EmbedListener extends Listener {
   public constructor(
@@ -19,19 +20,15 @@ export class EmbedListener extends Listener {
   }
 
   public async run(message: Message) {
-    const TARGET_USER_ID = "1359540812836376580";
-    if (message.author.id !== TARGET_USER_ID) return;
+    if (message.author.id !== process.env.DISCORD_CHANNEL_ID) return;
 
     if (!message.embeds || message.embeds.length === 0) return;
-    console.log(`[RECEIVED]: ${message.author.id}`);
 
     console.log(`Got an embed from ${message.author.tag}`);
     console.log(`[EMBEDS]: ${message.embeds}`);
     message.embeds.forEach((embed, i) => {
       console.log(`Embed #${i + 1}:`, embed.toJSON());
     });
-
-    console.log(`[DESCRIPTION]: ${message.embeds[0].description}`);
 
     let openAiResponse = "";
 
@@ -40,7 +37,12 @@ export class EmbedListener extends Listener {
       console.log(`[OPENAI RESPONSE]: ${openAiResponse}`);
 
       const channel = await message.client.channels.fetch(
-        "1359559780741677087"
+        process.env.DISCORD_CHANNEL_ID ??
+          (() => {
+            throw new Error(
+              "DISCORD_CHANNEL_ID is not set in the environment variables."
+            );
+          })()
       );
 
       if (
@@ -49,11 +51,13 @@ export class EmbedListener extends Listener {
         channel instanceof ThreadChannel
       ) {
         const embed = message.embeds[0].toJSON();
+        const nyTime = DateTime.now().setZone("America/New_York");
+
         embed.footer = {
-          text: `Today at ${new Date().toLocaleString("en-US", {
-            timeZone: "America/New_York",
-          })}`,
+          text: `🚨 Discord X Bot | NY Time: ${nyTime.toFormat("HH:mm:ss")}`,
         };
+
+        embed.timestamp = nyTime.toISO() ?? "";
 
         await channel.send({
           embeds: [embed],
